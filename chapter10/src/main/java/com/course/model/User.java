eeb114a0a258caa0d4757b307b1495181545544f
jiangzelin <964346145@qq.com>
2020-08-07 15:57:50 +0800

    java自动化测试例子

---
+++ chapter10/src/main/java/com/course/model/User.java
@@ -0,0 +1,11 @@
+package com.course.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String loginName;
+}
